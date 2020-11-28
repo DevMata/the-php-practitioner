01791ead90b57572156e725033939ec5ec7a090a
Antonio Mata <hildeamp@gmail.com>
2020-11-27 22:11:37 -0600

    refactor: move all files to todo folder

---
+++ todo/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router->define([
+    "todo" => "controllers/index.php",
+    "todo/about" => "controllers/about.php",
+    "todo/about/culture" => "controllers/about-culture.php",
+    "todo/contact" => "controllers/contact.php",
+]);
---
+++ todo/views/partials/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/todo/">Home</a></li>
+        <li><a href="/todo/about">About</a></li>
+        <li><a href="/todo/about/culture">About our Culture</a></li>
+        <li><a href="/todo/contact">Contact</a></li>
+    </ul>
+</nav>
