95e22b064aeaf107deaf90cc081c3ddb2e3a04d4
Antonio Mata <hildeamp@gmail.com>
2020-11-27 21:56:41 -0600

    feat: add head and footer to views

--- views/index.view.php
+++ views/index.view.php
@@ -1,31 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP</title>
-    <style>
-        header {
-            background-color: lightgray;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-<?php require("partials/nav.php"); ?>
-<ul>
-    <?php foreach ($tasks as $task): ?>
-        <li>
-            <?php if($task->completed): ?>
-                <strike><?= $task->description; ?></strike>
-            <?php else: ?>
-                <?= $task->description; ?>
-            <?php endif;?>
-        </li>
-    <?php endforeach;?>
-</ul>
-</body>
-</html>
+<?php require('partials/head.php'); ?>
+    <ul>
+        <?php foreach ($tasks as $task): ?>
+            <li>
+                <?php if ($task->completed): ?>
+                    <strike><?= $task->description; ?></strike>
+                <?php else: ?>
+                    <?= $task->description; ?>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php require("partials/footer.php"); ?>
