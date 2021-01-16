8820d8fc169142170b3c015ab374dc0a82eeb8c7
Antonio Mata <hildeamp@gmail.com>
2021-01-15 23:04:16 -0600

    feat: add view function

--- todo/core/bootstrap.php
+++ todo/core/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
 // I needed to resolve this path, I couldn't use relative path
-App::bind('config', require dirname(dirname(__FILE__))."/config.php");
+App::bind('config', require dirname(dirname(__FILE__)) . "/config.php");
 App::bind("database", new QueryBuilder(Connection::make(App::get("config")["database"])));
+
+function view($name, $data = [])
+{
+    extract($data);
+    return require "views/{$name}.view.php";
+}
