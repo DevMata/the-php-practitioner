0af1b49ac3756ea942117c3359af31081c3c02dc
Antonio Mata <hildeamp@gmail.com>
2021-01-21 20:57:14 -0600

    feat: use namespace path

--- todo/core/bootstrap.php
+++ todo/core/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Core\App;
+
 // I needed to resolve this path, I couldn't use relative path
 App::bind('config', require dirname(dirname(__FILE__)) . "/config.php");
 App::bind("database", new QueryBuilder(Connection::make(App::get("config")["database"])));
--- todo/index.php
+++ todo/index.php
@@ -3,5 +3,7 @@
 require 'vendor/autoload.php';
 require 'core/bootstrap.php';
 
+use App\Core\{Request, Router};
+
 Router::load('routes.php')
-    ->direct(Request::uri(),Request::method());
+    ->direct(Request::uri(), Request::method());
